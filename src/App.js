c21f00f6652cd225a04b6f03019eeebfe0bf551e
David Brown <gatech55@gmail.com>
2021-01-22 00:52:44 -0500

    updated the Hashrouter and basename

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
+import { HashRouter as Router, Route } from "react-router-dom";
 import Calculator from "./components/Calculator";
 
+// keep the basename below. DO NOT CHANGE
 function App() {
-  return <Calculator />;
+  return (
+    <Router basename="/">
+      <Calculator />
+    </Router>
+  );
 }
 
 export default App;
