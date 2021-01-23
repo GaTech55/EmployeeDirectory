beced175c991193b56a78bb9dfa7c3591fdcfe3c
David Brown <gatech55@gmail.com>
2021-01-22 23:19:25 -0500

    Added table with correct formatting

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import API from "../../utils/API";
 import axios from "axios";
 import Employee from "../../components/Employee/Employee";
 
@@ -6,25 +7,49 @@ const Home = () => {
   const [employee, setEmployee] = useState([]);
 
   useEffect(() => {
-    alert("Welcome user!");
-    axios.get("https://randomuser.me/api/?results=10").then((response) => {
-      console.log(response.data.results);
-      setEmployee(response.data.results);
+    API.getRandomEmployee().then((results) => {
+      console.log("results.data:", results.data);
+      setEmployee(results.data.results);
     });
   }, []);
 
   return (
-    <div className="container">
-      <div className="row">
-        {employee.map((employee) => (
-          <Employee
-            employeeUrl={employee.picture.large}
-            key={employee.id.value}
-            alt={employee.name.title}
-          />
-        ))}
+    <>
+      <nav className="navbar">
+        <div className="container">
+          <span className="h1">Employee Directory</span>
+        </div>
+      </nav>
+
+      <div className="container">
+        {/* <div className="row"> */}
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">Image</th>
+              <th scope="col">First Name</th>
+              <th scope="col">Last Name</th>
+              <th scope="col">Email</th>
+              <th scope="col">Cell</th>
+            </tr>
+          </thead>
+          <tbody>
+            {employee.map((employee) => (
+              <tr key={employee.cell}>
+                <td>
+                  <img src={employee.picture.thumbnail} />
+                </td>
+                <td>{employee.name.first}</td>
+                <td>{employee.name.last}</td>
+                <td>{employee.email}</td>
+                <td>{employee.cell}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        {/* </div> */}
       </div>
-    </div>
+    </>
   );
 };
 
---
+++ src/utils/API.js
@@ -0,0 +1,6 @@
+import axios from "axios";
+export default {
+  getRandomEmployee: function () {
+    return axios.get("https://randomuser.me/api/?results=50");
+  },
+};
